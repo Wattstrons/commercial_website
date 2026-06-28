74a45b96857cbb46dda7f90d1b4314bfdb112243
Wattstrons <wattstronsautomation@gmail.com>
2026-06-29 00:25:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/commercial_website/',
+  base: '/',   // ✅ IMPORTANT FIX
   plugins: [react(), tailwindcss()],
 })
