fce360f78d652a8abe5c3ec1fd6621b065cdf15e
Wattstrons <wattstronsautomation@gmail.com>
2026-08-13 21:10:24 +0530

    Update Question.jsx

--- src/components/Front/Question.jsx
+++ src/components/Front/Question.jsx
@@ -78,9 +78,8 @@ const Question = () => {
     };
 
     return (
-        <Section className="bg-transparent text-white relative z-10 !pt-6 lg:!pt-8 2xl:-mt-[150px] !pb-0 md:!pb-0 lg:!pb-0">
+        <Section className="bg-transparent text-white relative z-10 !pt-6 lg:!pt-8 2xl:mt-[10px] !pb-0 md:!pb-0 lg:!pb-0">
             <Container className="question-container">
-
 
 
                 {/* ================= HEADING ================= */}
